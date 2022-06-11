f2d053306415ede154d6764d096c9a28e251281a
Apple <apple@Apples-MacBook-Pro.local>
2022-06-11 17:53:41 +0530

    adding show issues files

--- src/components/MainIssuesContainer/MainIssuesContainer.js
+++ src/components/MainIssuesContainer/MainIssuesContainer.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import IssuesContainerLeft from './IssuesContainerLeft/IssuesContainerLeft'
 import IssuesContainerRight from './IssuesContainerRight/IssuesContainerRight'
-
 import "./MainIssuesContainer.css"
+import ShowIssues from './ShowIssues'
 
 function MainIssuesContainer() {
   return (
     <div className='main-issues-container'>
-        <IssuesContainerLeft/>
-        <IssuesContainerRight/>
+        <div className='main-issues-nav'>
+            <IssuesContainerLeft/>
+            <IssuesContainerRight/>
+        </div>
+        <div className='main-issues-display'>
+            <ShowIssues/>
+        </div>
     </div>
   )
 }
---
+++ src/components/MainIssuesContainer/ShowIssues.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./ShowIssues.css"
+
+function ShowIssues() {
+  return (
+    <div className='show-issues'>
+        <li>abcd</li>
+        <li>adfjs</li>
+        <li>nwf</li>
+        <li>kjfn</li>
+
+    </div>
+  )
+}
+
+export default ShowIssues
